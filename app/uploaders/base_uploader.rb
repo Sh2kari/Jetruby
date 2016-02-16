45429db2d4cfb658d1a49bda9f6e406cb79d64b8
Sh2kari <buba_sev@bk.ru>
2016-02-16 19:51:40 +0300

    [#113697791] delete files created by genarators, create base_uploader for other models, fix spaces in views

---
+++ app/uploaders/base_uploader.rb
@@ -0,0 +1,18 @@
+class BaseUploader < CarrierWave::Uploader::Base
+
+  include CarrierWave::MiniMagick
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def default_url
+    ActionController::Base.helpers.asset_path 'fallback/' + [model.class.to_s.underscore, mounted_as, version_name, "default.jpg"].compact.join('_')
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+end
