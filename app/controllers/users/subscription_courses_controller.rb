3cdd012fc60fb3ff84dc9dc5797c081edafa96af
Sh2kari <buba_sev@bk.ru>
2016-03-22 00:21:48 +0300

    [#115562913] User can see lessons only when subscribed

--- app/controllers/users/subscription_courses_controller.rb
+++ app/controllers/users/subscription_courses_controller.rb
@@ -2,6 +2,6 @@ class Users::SubscriptionCoursesController < Users::BaseController
   PER_PAGE = 8
 
   def index
-    @courses = @courses = current_user.participated_courses.recent.page(params[:page]).per(params[:per_page] || PER_PAGE).includes(:user)
+    @courses = current_user.participated_courses.recent.page(params[:page]).per(params[:per_page] || PER_PAGE)
   end
 end
--- app/models/dismiss.rb
+++
@@ -1,6 +0,0 @@
-class Dismiss < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :course
-
-  validates_uniqueness_of :course_id, scope: :user_id
-end
